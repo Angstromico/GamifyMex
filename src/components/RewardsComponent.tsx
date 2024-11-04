0dba589749dacf9ea39246fce31c1fa4da880e26
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-11-04 00:21:19 -0400

    Rewards component

--- src/components/RewardsComponent.tsx
+++ src/components/RewardsComponent.tsx
@@ -4,34 +4,49 @@ import { useTranslation } from 'react-i18next'
 const RewardsComponent = () => {
   const { t } = useTranslation()
 
+  // Debugging translation values
+  console.log(t('rewards.exclusiveBadge.title'))
+  console.log(t('rewards.leaderboardPoints.title'))
+  console.log(t('rewards.mysteryGift.title'))
+  console.log(t('rewards.weeklyMedal.title'))
+
+  /* This are the console.log: rewards.exclusiveBadge.title
+RewardsComponent.tsx:9 rewards.leaderboardPoints.title
+RewardsComponent.tsx:10 rewards.mysteryGift.title
+RewardsComponent.tsx:11 rewards.weeklyMedal.title
+hook.js:377 rewards.exclusiveBadge.title
+hook.js:377 rewards.leaderboardPoints.title
+hook.js:377 rewards.mysteryGift.title
+hook.js:377 rewards.weeklyMedal.title */
+
   // Array of rewards for illustration
   const rewards = [
     {
       icon: <FaStar size={28} />,
-      title: t('rewards.exclusiveBadge.title'),
-      description: t('rewards.exclusiveBadge.description'),
+      title: t('hero.rewards.exclusiveBadge.title'),
+      description: t('hero.rewards.exclusiveBadge.description'),
     },
     {
       icon: <FaTrophy size={28} />,
-      title: t('rewards.leaderboardPoints.title'),
-      description: t('rewards.leaderboardPoints.description'),
+      title: t('hero.rewards.leaderboardPoints.title'),
+      description: t('hero.rewards.leaderboardPoints.description'),
     },
     {
       icon: <FaGift size={28} />,
-      title: t('rewards.mysteryGift.title'),
-      description: t('rewards.mysteryGift.description'),
+      title: t('hero.rewards.mysteryGift.title'),
+      description: t('hero.rewards.mysteryGift.description'),
     },
     {
       icon: <FaMedal size={28} />,
-      title: t('rewards.weeklyMedal.title'),
-      description: t('rewards.weeklyMedal.description'),
+      title: t('hero.rewards.weeklyMedal.title'),
+      description: t('hero.rewards.weeklyMedal.description'),
     },
   ]
 
   return (
     <div className='text-center'>
       <h2 className='text-xl font-bold mb-4'>
-        {t('rewards.availableRewards')}
+        {t('hero.rewards.availableRewards')}
       </h2>
       <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
         {rewards.map((reward, index) => (
